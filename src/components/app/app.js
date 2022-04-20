4f5df6c2bf1721de937dba6811cc3e69363df800
Qobil Urinboev <qobilurinboev98@gmail.com>
2022-04-21 00:25:50 +0500

    form add and delete

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,61 @@
+import {Component} from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
-import EmployersAddForm from '../employers-add-form/employers.add-form';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
 import './app.css';
 
-function App () {
-  const data = [
-    {name:"John C.",salary: 1000, increase: false, id: 1},
-    {name:"Alex H.",salary: 2700,  increase: false, id: 2},
-    {name:"Carl Y.",salary: 1400, increase: true, id: 3},
-  ];
+class App extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      data: [
+        {name: 'John C.', salary: 1000, increase: false, id: 1},
+        {name: 'Alex H.', salary: 2700, increase: false, id: 2},
+        {name: 'Carl Y.', salary: 1400, increase: true, id: 3},
+      ],
+    };
+    this.maxId = 4;
+  }
 
-
-
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  deleteItem = id => {
+    this.setState (({data}) => {
+      return {
+        data: data.filter (item => item.id !== id),
+      };
+    });
+  };
+  addItem = (name, salary) => {
+    const newItem = {
+        name, 
+        salary,
+        increase: false,
+        id: this.maxId++
+    }
+    this.setState(({data}) => {
+        const newArr = [...data, newItem];
+        return {
+            data: newArr
+        }
+    });
+}
+  render () {
+    const {data} = this.state;
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployersList data={data} onDelete={id => this.deleteItem (id)} />
+        <EmployersAddForm onAdd={this.addItem} />
       </div>
-      <EmployersList data={data}/>
-      <EmployersAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,30 +1,62 @@
+import {Component} from 'react';
 import './employers-list-item.css';
 
-const EmployeesListItem = ({name, salary, increase}) => {
-    let classNames = 'list-group-item d-flex justify-content-between'
-    if(increase){
-        classNames += ' increase'
+class EmployeesListItem extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      increase: false,
+      like: false,
+    };
+  }
+  onIncrease = () => {
+    this.setState (({increase}) => ({
+      increase: !increase,
+    }));
+  };
+  onLike = () => {
+    this.setState (({like}) => ({
+      like: !like,
+    }));
+  };
+  render () {
+    const {name, salary, onDelete} = this.props;
+    const {increase, like} = this.state;
+    let classNames = 'list-group-item d-flex justify-content-between';
+    if (increase) {
+      classNames += ' increase';
     }
-  return (
-    <li className={classNames}>
-      <span className="list-group-item-label">{name}</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary + '$'}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm ">
-          <i className="fas fa-cookie" />
-        </button>
+    if(like){
+      classNames += ' like'
+    }
+    return (
+      <li className={classNames}>
+        <span onClick={this.onLike} className="list-group-item-label">
+          {name}
+        </span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + '$'}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm "
+            onClick={this.onIncrease}
+          >
+            <i className="fas fa-cookie" />
+          </button>
 
-        <button type="button" className="btn-trash btn-sm ">
-          <i className="fas fa-trash" />
-        </button>
-        <i className="fas fa-star" />
-      </div>
-    </li>
-  );
-};
+          <button type="button" className="btn-trash btn-sm ">
+            <i className="fas fa-trash" 
+            onClick={onDelete}/>
+          </button>
+          <i className="fas fa-star" />
+        </div>
+      </li>
+    );
+  }
+}
 
 export default EmployeesListItem;
